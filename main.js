f52e374f7541def64a3e84188977482af9ded42b
Afizahmad Fairuz <afizahmadfairuz@gmail.com>
2024-06-09 19:52:13 +0700

    feat: Update index.html and main.js
    - Modify index.html to include new content
    - Update main.js to fix bug in function foo()

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
+
+
+
