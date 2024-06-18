379c733c20723f27c6f6cf31e7a8835fc9145d7e
Afizahmad Fairuz <afizahmadfairuz@gmail.com>
2024-06-18 19:45:10 +0700

    update

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = 8000;
 
 // Mengimpor repository
 const UserRepository = require("./auth_backend/repository/UserRepository");
 const ProductRepository = require("./auth_backend/repository/ProductRepository");
 const CategoryRepository = require("./auth_backend/repository/CategoryRepository");
-const OrderRepository = require("./auth_backend/repository/OrderRepository"); 
+const OrderRepository = require("./auth_backend/repository/OrderRepository");
 
 // Mengimpor service
 const UserService = require("./auth_backend/service/UserService");
 const ProductService = require("./auth_backend/service/ProductService");
 const CategoryService = require("./auth_backend/service/CategoryService");
-const OrderService = require("./auth_backend/service/OrderService"); 
+const OrderService = require("./auth_backend/service/OrderService");
 
 // Mengimpor handler
 const UserHandler = require("./auth_backend/handler/UserHandler");
 const ProductHandler = require("./auth_backend/handler/ProductHandler");
 const CategoryHandler = require("./auth_backend/handler/CategoryHandler");
-const OrderHandler = require("./auth_backend/handler/OrderHandler"); 
+const OrderHandler = require("./auth_backend/handler/OrderHandler");
 
 // Middleware untuk parsing request body
 app.use(express.json());
@@ -44,19 +45,19 @@ app.use(internalServerErrorHandler);
 const userRepository = new UserRepository();
 const productRepository = new ProductRepository();
 const categoryRepository = new CategoryRepository();
-const orderRepository = new OrderRepository(); 
+const orderRepository = new OrderRepository();
 
 // Inisialisasi service
 const userService = new UserService(userRepository);
 const productService = new ProductService(productRepository, userRepository);
 const categoryService = new CategoryService(categoryRepository);
-const orderService = new OrderService(orderRepository); 
+const orderService = new OrderService(orderRepository);
 
 // Inisialisasi handler
 const userHandler = new UserHandler(userService);
 const productHandler = new ProductHandler(productService);
 const categoryHandler = new CategoryHandler(categoryService);
-const orderHandler = new OrderHandler(orderService); 
+const orderHandler = new OrderHandler(orderService);
 
 // Route untuk User
 app.get("/users", userHandler.getAll);
@@ -77,6 +78,11 @@ app.get("/orders", orderHandler.getAll);
 app.get("/orders/:id", orderHandler.getById);
 app.post("/orders", orderHandler.create);
 app.delete("/orders/:id", orderHandler.deleteById);
+
+// Endpoint untuk menampilkan gambar
+app.get("/images/binar.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "assets", "binar.png"));
+});
 
 // Menjalankan server
 app.listen(PORT, () => {
